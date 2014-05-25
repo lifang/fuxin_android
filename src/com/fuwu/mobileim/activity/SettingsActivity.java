4bc43de0f07cfe77abbee6c645085e84d49ff50e
dingzuoqiang <530858106@qq.com>
2014-05-25 09:24:51 +0800

    dzq

--- src/com/fuwu/mobileim/activity/SettingsActivity.java
+++ src/com/fuwu/mobileim/activity/SettingsActivity.java
@@ -1,4 +1,4 @@
-package com.fuwu.mobileim.activity;
+﻿package com.fuwu.mobileim.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -7,7 +7,7 @@ import com.fuwu.mobileim.R;
 
 /**
  * @作者 马龙
- * @时间 创建时间：2014-5-14 下午12:06:40
+ * @时间  创建时间：2014-5-14 下午12:06:40
  */
 public class SettingsActivity extends Activity {
 
