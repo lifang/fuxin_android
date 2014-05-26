3bbd9bf4ee999e8d23edeffaefd59b455625ec20
zxn <305845671@qq.com>
2014-05-26 09:06:36 +0800

    zxn

--- src/com/fuwu/mobileim/util/FuXunTools.java
+++ src/com/fuwu/mobileim/util/FuXunTools.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class FuXunTools {
 
 	// 判断手机号合法性
-	public boolean isMobileNO(String mobiles) {
+	public static boolean isMobileNO(String mobiles) {
 		Pattern p = Pattern
 				.compile("^((13[0-9])|(15[^4,\\D])|(18[0-9]))\\d{8}$");
 		Matcher m = p.matcher(mobiles);
