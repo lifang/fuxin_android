68e1061a0c0a0bc24eada057bdc28149fad13097
dingzuoqiang <530858106@qq.com>
2014-05-30 15:43:28 +0800

    dzq

--- src/com/fuwu/mobileim/util/Urlinterface.java
+++ src/com/fuwu/mobileim/util/Urlinterface.java
@@ -8,7 +8,7 @@ public interface Urlinterface {
 	static final String tag = "FuXun";
 	static final String SHARED = "FuXun";
 	static final double current_version = 0.0;// 应用版本号
-	static final String IP = "https://118.242.18.189/IMApi/";
+	static final String IP = "https://118.242.18.189/IMApiMock/";
 	// static final String IP = "https://118.242.18.189/IMApiMock";
 
 	String getContacts = IP + "api/Contact";// 获得联系人
