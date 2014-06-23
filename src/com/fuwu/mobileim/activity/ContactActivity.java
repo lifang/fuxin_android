bc9b322ede7e727a08ef9d467f54ceacd214a282
malong <286753213@qq.com>
2014-06-23 09:51:34 +0800

    malong

--- src/com/fuwu/mobileim/activity/ContactActivity.java
+++ src/com/fuwu/mobileim/activity/ContactActivity.java
@@ -63,7 +63,7 @@ public class ContactActivity extends Fragment implements IXListViewListener {
 	private FxApplication fxApplication;
 	private XListView xListView;// 可上拉刷新 的 listview ，
 	private TextView dialog;
-	private ContactAdapter  adapter2;
+	private ContactAdapter adapter2;
 	private View rootView;
 	private int user_number1 = 0;
 	List<ShortContactPojo> contactsList1 = new ArrayList<ShortContactPojo>();
@@ -125,7 +125,7 @@ public class ContactActivity extends Fragment implements IXListViewListener {
 						}
 					}
 					FuXunTools.getBitmap(contactsList);
-						
+
 				}
 				switchButton(buttonNumber);
 				onLoad();
@@ -170,7 +170,7 @@ public class ContactActivity extends Fragment implements IXListViewListener {
 		// TODO Auto-generated method stub
 		super.onStart();
 		Log.i("11", "-----------");
-		if (buttonNumber==0) {
+		if (buttonNumber == 0) {
 			handler.sendEmptyMessage(0);
 		}
 	}
@@ -181,7 +181,6 @@ public class ContactActivity extends Fragment implements IXListViewListener {
 	 * 
 	 * 
 	 */
-
 
 	private void getContacts2() {
 		ExecutorService singleThreadExecutor = Executors
@@ -286,7 +285,6 @@ public class ContactActivity extends Fragment implements IXListViewListener {
 	}
 
 	private void initViews() {
-
 		xListView = (XListView) rootView
 				.findViewById(R.id.contacts_list_view_refresh);
 		xListView.setDivider(null);
@@ -336,13 +334,13 @@ public class ContactActivity extends Fragment implements IXListViewListener {
 		if (height == 1280 && width == 720) {
 			hight0 = 70;
 			param.leftMargin = width1;
-			param.rightMargin = width1+1;
+			param.rightMargin = width1 + 1;
 		} else if (height == 854 && width == 480) {
 			hight0 = 45;
 			width1 = 18;
 			width0 = 2;
 			param.leftMargin = width1;
-			param.rightMargin = width1-1;
+			param.rightMargin = width1 - 1;
 		} else if (height == 1920 && width == 1080) {
 			width1 = 40; // 外部边框距左右边界距离
 			hight0 = 100;
@@ -409,9 +407,8 @@ public class ContactActivity extends Fragment implements IXListViewListener {
 		}
 	};
 
-	
 	private void switchButton(int buttonNumber) {
-		
+
 		switch (buttonNumber) {
 		case 0:
 			setButtonColor(buttonNumber);
@@ -428,7 +425,7 @@ public class ContactActivity extends Fragment implements IXListViewListener {
 					contactsList.add(contactsList1.get(i));
 				}
 			}
-//			Collections.sort(contactsList, pinyinComparator);
+			// Collections.sort(contactsList, pinyinComparator);
 			xListView.setAdapter(adapter2);
 			adapter2.updateListView(contactsList);
 			break;
@@ -459,7 +456,8 @@ public class ContactActivity extends Fragment implements IXListViewListener {
 					contactsList.add(contactsList1.get(i));
 				}
 			}
-			Log.i("linshi", "----1-----2-----3---------------------------------");
+			Log.i("linshi",
+					"----1-----2-----3---------------------------------");
 			Collections.sort(contactsList, longDataComparator);
 			xListView.setAdapter(adapter2);
 			adapter2.updateListView(contactsList);
@@ -468,10 +466,9 @@ public class ContactActivity extends Fragment implements IXListViewListener {
 		default:
 			break;
 		}
-		
-		
+
 	}
-	
+
 	private void setButtonColor(int buttonNumber) {
 		for (int i = 0; i < btnList.size(); i++) {
 			if (buttonNumber == i) {
@@ -521,15 +518,15 @@ public class ContactActivity extends Fragment implements IXListViewListener {
 	}
 
 	public void onRefresh() {
-			if (FuXunTools.isConnect(getActivity())) {
+		if (FuXunTools.isConnect(getActivity())) {
 
-				// Thread thread = new Thread(new getContacts2());
-				// thread.start();
-				getContacts2();
-			} else {
-				Toast.makeText(getActivity(), R.string.no_internet,
-						Toast.LENGTH_SHORT).show();
-			}
+			// Thread thread = new Thread(new getContacts2());
+			// thread.start();
+			getContacts2();
+		} else {
+			Toast.makeText(getActivity(), R.string.no_internet,
+					Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	@Override
